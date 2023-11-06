2141047d9e80ff9c4a6d57ab3c9f047ff439f648
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-06 21:45:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let line = document.getElementById("line");
+setInterval(lineRotate, 20);
+let angle = 0;
+function lineRotate() {
+    angle = angle + 2;
+    if(angle == 360){
+        angle = 0;
+    }
+	line.style.transform = `rotate(${angle}deg)`;
+}
